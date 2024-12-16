e71d201954f2c02773068b86afd45e3699d59180
kGB <116108379+gibosio@users.noreply.github.com>
2024-12-16 10:15:12 +0100

    refactor test add sender
    //TODO cannot access private field because test is not a submodule of drone

--- tests/test_handle_command.rs
+++ tests/test_handle_command.rs
@@ -1,31 +1,45 @@
 use rustastic_drone::RustasticDrone;
 
 use crossbeam_channel::unbounded;
-use std::{collections::HashMap, thread, time::Duration};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
 use wg_2024::{controller::DroneCommand, drone::Drone};
 
 #[test]
 fn test_add_sender() {
-    let (send_to_drone, receive_from_drone) = unbounded();
+    let (drone_to_controller, _controller_from_drone) = unbounded();
+    let (controller_to_drone, drone_from_controller) = unbounded();
 
-    let mut drone = RustasticDrone::new(
-        0,
-        unbounded().0,
-        receive_from_drone,
+    let drone_thread = Arc::new(Mutex::new(RustasticDrone::new(
+        1,
+        drone_to_controller.clone(),
+        drone_from_controller,
         unbounded().1,
         HashMap::new(),
         0f32,
-    );
-    let join = thread::spawn(move || drone.run());
+    )));
+    println!("{drone_thread:?}");
+    let drone = drone_thread.clone();
 
-    send_to_drone
-        .send(DroneCommand::AddSender(1, unbounded().0))
+    let handler = thread::spawn(move || drone_thread.lock().unwrap().run());
+
+    controller_to_drone
+        .send(DroneCommand::AddSender(15, unbounded().0))
         .unwrap();
+    thread::sleep(Duration::from_secs(1));
 
-    thread::sleep(Duration::from_secs(2));
-    send_to_drone.send(DroneCommand::Crash).unwrap();
-    join.join().unwrap();
+    controller_to_drone.send(DroneCommand::Crash).unwrap();
+    handler.join().unwrap();
+    let drone = drone.lock().unwrap();
+    println!("{drone:?}");
+    //TODO cannot access private field because test is not a submodule of drone
+    // assert_ne!(drone.packet_send.iter().last().0, 15);
 }
+
 #[test]
 fn test_set_pdr() {
     let (send_to_drone, receive_from_drone) = unbounded();
