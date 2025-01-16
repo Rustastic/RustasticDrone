1d9d6c1b05f17e36905c914377af0702fda845ab
Andrea Carzeri <101599788+AndreaCarzeri@users.noreply.github.com>
2025-01-16 13:56:16 +0100

    Update drone.rs

--- src/drone.rs
+++ src/drone.rs
@@ -454,7 +454,7 @@ impl RustasticDrone {
                 self.send_nack(packet, None, NackType::Dropped);
             }
         } else {
-            if packet.routing_header.hop_index = packet.routing_header.hops.len() - 1 {
+            if packet.routing_header.hop_index == packet.routing_header.hops.len() - 1 {
                 error!(
                     "{} Invalid hop index increment detected in [ Drone: {} ] for header of Packet [ session_id: {} ]",
                     "✗".red(),
